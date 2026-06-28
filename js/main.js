7dea3abe05eb5d2265d0165617088dd86dd3b27a
Brandon Carter <brcarter1@student.fullsail.edu>
2026-06-27 21:32:56 -0500

    updated js file

--- js/main.js
+++ js/main.js
@@ -4,4 +4,38 @@
 
 // IIFE also has been added to start the process
 
-(() => {})();
+class Employee {
+  constructor(name, age, payRate, hours) {
+    this.name = name;
+    this.age = age;
+    this.payRate = this.payRate;
+    this.hours = hours;
+
+    this.annualSalary = 0;
+    this.employeeType = "Employee";
+  }
+}
+// manager class that extends emp class
+class Manager extends Employee {
+  constructor(name, age, payRate, hours) {
+    super(name, age, payRate, hours);
+    this.employeeType = "Manager";
+    this.calcPay();
+  }
+
+  calcPay() {
+    this.annualSalary = this.hours * this.payRate * 52 - 1000;
+  }
+}
+
+class PartTime extends Employee {
+  constructor(name, age, payRate, hours) {
+    super(name, age, payRate, hours);
+    this.employeeType = "Part Time";
+    this.calcPay();
+  }
+
+  calcPay() {
+    this.annualSalary = this.hours * this.payRate * 52;
+  }
+}
